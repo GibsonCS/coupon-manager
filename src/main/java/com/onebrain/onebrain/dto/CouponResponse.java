06153c0abd0b15a56335858b6ac6ae3e9043538b
GibsonCS <gibson.cruzds@gmail.com>
2026-03-07 00:02:11 -0300

    fix: chage atribute type from coupon response dto

--- src/main/java/com/onebrain/onebrain/dto/CouponResponse.java
+++ src/main/java/com/onebrain/onebrain/dto/CouponResponse.java
@@ -3,14 +3,15 @@ package com.onebrain.onebrain.dto;
 import com.onebrain.onebrain.domain.CouponStatus;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.UUID;
 
 public record CouponResponse(
-        UUID id, String code,
+        UUID id,
+        String code,
         String description,
         BigDecimal discountValue,
-        Instant expirationDate,
-        CouponStatus status, boolean published,
+        String expirationDate,
+        CouponStatus status,
+        boolean published,
         boolean redeemed) {
 }
