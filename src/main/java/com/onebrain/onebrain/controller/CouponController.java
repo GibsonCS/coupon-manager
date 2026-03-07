e8e7daa3b3669f46ba1427140534b997847afdd2
GibsonCS <gibson.cruzds@gmail.com>
2026-03-07 00:08:22 -0300

    feat: create coupon controller

---
+++ src/main/java/com/onebrain/onebrain/controller/CouponController.java
@@ -0,0 +1,31 @@
+package com.onebrain.onebrain.controller;
+
+import com.onebrain.onebrain.dto.CouponRequest;
+import com.onebrain.onebrain.dto.CouponResponse;
+import com.onebrain.onebrain.service.CouponService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coupon")
+public class CouponController {
+
+    private final CouponService couponService;
+
+    public CouponController(CouponService couponService) {
+        this.couponService = couponService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CouponResponse> create(@Valid @RequestBody CouponRequest couponRequest) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(couponService.create(couponRequest));
+    }
+}
