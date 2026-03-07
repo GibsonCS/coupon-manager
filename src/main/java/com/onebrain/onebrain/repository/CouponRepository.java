40238b9218b3e57a0d24dc08784d2d40d98105ae
GibsonCS <gibson.cruzds@gmail.com>
2026-03-07 00:03:16 -0300

    feat: change return type to optional

--- src/main/java/com/onebrain/onebrain/repository/CouponRepository.java
+++ src/main/java/com/onebrain/onebrain/repository/CouponRepository.java
@@ -3,7 +3,9 @@ package com.onebrain.onebrain.repository;
 import com.onebrain.onebrain.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    public Optional<Coupon> findByCode(String code);
 }
